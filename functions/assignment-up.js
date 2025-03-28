09a1e465ef9fdad1b6e553e5d484e9438120fd7b
Sudeep  Shivashettar <sudeepshivashettar@gmail.com>
2025-03-29 02:23:39 +0530

    t15

--- functions/assignment-up.js
+++ functions/assignment-up.js
@@ -31,10 +31,10 @@ exports.handler = async (event) => {
 
     // Azure SQL Connection
     const pool = await sql.connect({
-      user: process.env.AZURE_SQL_USER,
-      password: process.env.AZURE_SQL_PASSWORD,
-      server: process.env.AZURE_SQL_SERVER,
-      database: process.env.AZURE_SQL_DATABASE,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      server: process.env.DB_SERVER,
+      database: process.env.DB_NAME,
       options: { encrypt: true }
     });
 
